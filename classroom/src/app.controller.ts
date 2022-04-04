2e9a4969ab3e90fafb5a0f8d10f0865b4aa46b72
Luiz Henrique <7henrique18@gmail.com>
2022-04-04 11:53:57 -0300

    :sparkles: Setup Auth0 on purschases back-end

--- classroom/src/app.controller.ts
+++ classroom/src/app.controller.ts
@@ -3,8 +3,6 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
     return "Hello World! This's a classroom app!";
