6ea055093a32a00bd697e0e797cf62cd24e157e9
Luiz Henrique <7henrique18@gmail.com>
2022-05-07 23:20:58 -0300

    :atom: Setup GraphQL client and codegen types

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -4,25 +4,40 @@ import {
   withPageAuthRequired,
 } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
+import { withApollo } from "../../lib/withApollo";
+import {
+  useGetProductsQuery,
+  useMeQuery,
+} from "../../graphql/generated/graphql";
+import { getServerPageGetProducts } from "../../graphql/generated/page";
 
 const Home = () => {
   const { user } = useUser();
+
+  const { data: me } = useMeQuery();
 
   return (
     <>
       <h1>Olá, {user?.name}!</h1>
 
-      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <pre>ok: {JSON.stringify(me, null, 2)}</pre>
+
+      <div className="bg-buffy">
+        <p className="text-nosferatu ">I vant to suck your blood...</p>
+        <pre>{JSON.stringify(user, null, 2)}</pre>
+      </div>
     </>
   );
 };
 
-export default Home;
-
 export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
-  getServerSideProps: async ({ req, res }) => {
-    console.log(getAccessToken(req, res));
+  getServerSideProps: async (ctx) => {
+    // getServerPageGetProducts({}, ctx);
 
-    return { props: {} };
+    return {
+      props: {},
+    };
   },
 });
+
+export default withApollo(Home);
